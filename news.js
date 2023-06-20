a379fb32025dea9c7ab8348b75450ca7a5fb7b05
insectty <126439658+insectty@users.noreply.github.com>
2023-06-20 21:39:35 +0800

    Add files via upload

---
+++ news.js
@@ -0,0 +1,37 @@
+// variables
+const generalBtn = document.getElementById("genral");
+const businessBtn = document.getElementById("business");
+const sportsBtn = document.getElementById("sports");
+const entertaimentBtn = document.getElementById("entertaiment");
+const technologyBtn = document.getElementById("technology");
+const searchBtn = document.getElementById("searchBtn");
+
+const newsQuery = document.getElementById("newsQuery");
+const newsType = document.getElementById("newsType");
+const newsdetails = document.getElementById("newsdetails")
+
+// apis
+
+generalBtn.addEventListener("click",function(){
+
+});
+
+businessBtn.addEventListener("click",function(){
+
+});
+
+sportsBtn.addEventListener("click",function(){
+
+});
+
+entertaimentBtn.addEventListener("click",function(){
+
+});
+
+technologyBtn.addEventListener("click",function(){
+
+});
+
+searchBtn.addEventListener("click",function(){
+
+});
